1bd36e3c6524ba50dbc0d8c02ef9b07f9b29815a
micessien <micaeldie@gmail.com>
2022-10-04 21:44:38 +0000

    Manage templating by ejs

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
-
-const pdfTemplate = require('./documents');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
@@ -15,18 +15,32 @@ app.use(bodyParser.json());
 
 // POST - PDF generation and fetching of the data
 app.post('/create-pdf', (req, res) => {
-    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
-        if(err) {
-            res.send(Promise.reject());
-        }
+    // Get today's date
+    const today = new Date();
+    // structure data
+    const dataReceived = {
+        ...req.body,
+        today
+    }
 
-        res.send(Promise.resolve());
+    ejs.renderFile(path.join(__dirname, './documents/index.ejs'), dataReceived, (err, data) => {
+        if (err) {
+            res.send(err)
+        }else{
+            pdf.create(data, {format: 'Letter'}).toFile('pdf/result.pdf', (err) => {
+                if(err) {
+                    res.send(Promise.reject());
+                }
+
+                res.send(Promise.resolve());
+            });
+        }
     });
 });
 
 // GET - Send the generated PDF to the client
 app.get('/fetch-pdf', (req, res) => {
-    res.sendFile(`${__dirname}/result.pdf`)
+    res.sendFile(`${__dirname}/pdf/result.pdf`)
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
